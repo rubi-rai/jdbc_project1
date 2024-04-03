3324b75cf45dbc0c027d06cec7f8660318100109
rubi-rai <rubi.rai2227@gmail.com>
2024-04-03 18:29:58 +0530

    java messages

---
+++ src/jdbc_project1/Welcome2.java
@@ -0,0 +1,11 @@
+package jdbc_project1;
+
+public class Welcome2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("welcome to java ");
+System.out.println("hello world");
+	}
+
+}
