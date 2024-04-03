50dd73d6a64ccf9d1b9ac3847d28c63d43868e46
rubi-rai <rubi.rai2227@gmail.com>
2024-04-03 18:25:22 +0530

    added

---
+++ src/jdbc_project1/Welcome.java
@@ -0,0 +1,12 @@
+package jdbc_project1;
+
+public class Welcome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("welcome to java");
+		System.out.println("hii java");
+
+	}
+
+}
